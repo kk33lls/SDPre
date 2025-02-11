59627ed491836995ee065bdca32dab986fb622fb
Kirstin <kirstin.eells98@gmail.com>
2025-02-10 20:05:03 -0700

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Kirstin";
+    int age = 26;
+    String state = "Kansas";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}

f0809e46c3df1bc926d36da4a435d3c110670252
Kirstin <kirstin.eells98@gmail.com>
2025-02-10 21:09:49 -0700

    complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,12 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1 = 15;
+    double num2 = 6;
+    double num3 = 2;
+    double sum = num1 + num2 + num3;
+    double average = sum / 3;
+   
+
+    System.out.println(average);
+  }
+}
